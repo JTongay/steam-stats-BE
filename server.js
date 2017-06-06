df33a4e0cb31904b5a5ccf722a3b19fbc1470825
Joseph Tongay <dirk.tongay@gmail.com>
2017-06-05 20:40:25 -0500

    allowing cors for all sites for now, go back and fix later on

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ app.use(bodyParser.json());
 app.use(express.static('dist'))
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //use dem routes
 app.use(loginRoutes)
